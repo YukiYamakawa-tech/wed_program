f43ef2c0aaceae06a328235d8ad0cd3e47ba6a25
YukiYamakawa-tech <143375737+YukiYamakawa-tech@users.noreply.github.com>
2024-05-09 08:49:46 +0900

    Add authentication practice and update Pipfile
    A new Python file 'auth_practice.py' has been added to implement user authentication using hashing functions. The Pipfile has also been updated to include necessary packages and set Python version to "3.11".

---
+++ auth_practice.py
@@ -0,0 +1,65 @@
+import re
+import urllib
+import urllib.request
+import urllib.error
+import hashlib
+import uuid
+from pprint import pprint
+
+
+def digest(s):
+    return hashlib.md5(s.encode("ascii")).hexdigest()
+
+
+def https_digest_test(url, auth_data=None):
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Charset": "utf-8",
+        "Accept-Language": "ja",
+    }
+    if auth_data is not None:
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Accept-Charset": "utf-8",
+            "Accept-Language": "ja",
+            "Authorization": auth_data,
+        }
+
+    req = urllib.request.Request(url, headers=headers, method="GET")
+    try:
+        with urllib.request.urlopen(req) as resp:
+            print("URL :", resp.geturl())
+            print("Code :", resp.getcode())
+            print("Content-Type :", resp.info()["content-type"])
+            body = resp.read()
+            return body
+    except urllib.error.HTTPError as e:
+        print(e)
+        err_headers = e.headers
+        print("WWW-Authenticate :", err_headers["WWW-Authenticate"])
+        pat = re.compile(
+            r"Digest realm=\"(?P<realm>\S+?)\", nonce=\"(?P<nonce>\S+?)\", qop=\"(?P<qop>\S+?)\", opaque=\"(?P<opaque>\S+?)\", algorithm=(?P<algorithm>\S+?), stale=(?P<stale>\S+?)"
+        )
+        match = pat.fullmatch(err_headers["WWW-Authenticate"])
+        return match
+
+
+
+if __name__ == '__main__':
+    url = "https://httpbin.org/digest-auth/auth/myname/mypass"
+    user = "myname"
+    passwd = "mypass"
+    uri = "/digest-auth/auth/myname/mypass"
+    match = https_digest_test(url)
+    ha1 = digest(f"{user}:{match['realm']}:{passwd}")
+    ha2 = digest(f"GET:{uri}")
+    print(f"hash #1: {ha1}")
+    print(f"hash #2: {ha2}")
+    nonce_count = 1
+    cnonce = str(uuid.uuid4()).replace("-", "")
+    dig = digest(f"{ha1}:{match['nonce']}:{nonce_count:08d}:{cnonce}:auth:{ha2}")
+    print(f"dig: {dig}")
+    auth_data = f"Digest username=\"{user}\", realm=\"{match['realm']}\", nonce=\"{match['nonce']}\", uri=\"{uri}\", response=\"{dig}\", opaque=\"{match['opaque']}\", qop={match['qop']}, nc={nonce_count:08d}, cnonce=\"{cnonce}\""
+    print(auth_data)
+    body = https_digest_test(url, auth_data)
+    pprint(body.decode("utf-8"))
---
+++ regular_expression.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import regex as re
+import pathlib
+
+def get_text(url):
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.find('div', {'class': 'main_text'}).text
+    file_path_name = '吾輩は猫である.txt'
+    file_path = pathlib.Path(file_path_name).absolute()
+    with open(file_path, 'w') as f:
+        f.write(text)
+    return file_path
+
+
+def get_idiom(text):
+    text = text.replace('\n', '').replace('\r', '').replace(' ', '')
+    pattern = r'\p{Script=Han}{4}(?!\p{Script=Han})'
+    matches = re.findall(pattern, text)
+    print(matches)
+    print(len(matches))
+    unique_matches = list(set(matches))
+    match_count = len(unique_matches)
+    print(match_count)
+    return None
+
+
+def get_longest_idiom(text):
+    text = text.replace('\n', '').replace('\r', '').replace(' ', '')
+    pattern = r'\p{Script=Han}+'
+    matches = re.findall(pattern, text)
+    max_len = max([len(match) for match in matches])
+    longest_words = [match for match in matches if len(match) == max_len]
+    print("最長の熟語は{}文字で、その熟語は：{}".format(max_len, '、'.join(longest_words)))
+    return None
+
+
+if __name__ == '__main__':
+    url = 'https://www.aozora.gr.jp/cards/000148/files/789_14547.html'
+    path = get_text(url)
+    with open(path, 'r') as f:
+        text = f.read()
+    get_idiom(text)
+    get_longest_idiom(text)
+
