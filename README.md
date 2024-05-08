bd7785564e8b7f9db90048eb5e3d00ae497d3255
YukiYamakawa-tech <143375737+YukiYamakawa-tech@users.noreply.github.com>
2024-05-09 08:48:36 +0900

    Add README, .gitignore, index.html, style.css, and http_req.py files
    This commit introduces a README file for repository setup instructions, an index.html for a personal introduction page, and a style.css for styling the HTML page. A .gitignore file is created to exclude unnecessary files and directories from version control. Also, a new Python script (http_req.py) has been added for making HTTP requests.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Wed_program
+
+一橋大学 プログラミング授業課題を扱い提出するためのリポジトリです。
+
+## 環境
+- Python 3.11
+- pipenv 
+- pip
+
+## ローカル実行
+```shell
+# cd /path/to/Wed_program
+pipenv run [script_name]
+```
+
+   
---
+++ image/http_req.py
@@ -0,0 +1,39 @@
+from pprint import pprint
+
+import requests
+
+
+def detect_language(text):
+    url = "http://127.0.0.1:5000/detect"
+    params = {"q": text}
+    response = requests.post(url, data=params)
+    print(response.json())
+    return response.json()
+
+
+def translate_text(text, dest):
+    url = "http://127.0.0.1:5000/translate"
+    params = {
+        "q": text,
+        "source": "auto",
+        "target": dest,
+        "format": "text"
+    }
+    response = requests.post(url, data=params)
+    return response.json()
+
+
+if __name__ == '__main__':
+    # テキストの各部分を個別に識別して翻訳する
+    # Mount Fuji is the highest mountain in Japan
+    # a main Japanese castle is Tokyo Mont Blanc is the highest mountain in Europe?
+    # what is the country's capital city where the highest mountain in the world is located?
+    r= "O Monte Fuji é a montanha mais alta do Japão a glavni grad Japana je Tokio Mont Blanc je najvišja gora v Evropi ano ang kabiserang lungsod ng bansa kung saan matatagpuan ang pinakamataas na bundok sa mundo?"
+    texts = "ano ang kabiserang lungsod ng bansa kung saan matatagpuan ang pinakamataas na bundok sa mundo"
+    translations = []
+    languages = []
+    lang = detect_language(texts)[0]['language']
+    languages.append(lang)
+    translated_text = translate_text(texts, 'en')
+    translations.append(translated_text['translatedText'])
+    print(translations)
